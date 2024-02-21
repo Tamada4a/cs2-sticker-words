19f3e7528377dae5f4f8d7ecaac4cb6d49f52cf3
Kirill Simovin <volce.chat@mail.ru>
2024-02-21 12:12:59 +0400

    Add ability to go to sticker page in the market

--- frontend/src/components/StickersCol/StickersCol.jsx
+++ frontend/src/components/StickersCol/StickersCol.jsx
@@ -2,11 +2,20 @@ import "./StickersCol.css";
 import { Stack, Typography } from "@mui/material";
 
 function StickersCol({ col }) {
+    function getStickerMarketLink(stickerName) {
+        return `https://steamcommunity.com/market/listings/730/${encodeURI(stickerName)}`;
+    }
+
+
     return (
         <Stack direction="column" spacing={"15px"} alignItems={"center"}>
             <Typography color={"black"} fontSize={24}>{col.colName}</Typography>
             <Stack direction="column" spacing={"10px"} alignItems={"center"} className="stickers-col">
-                {col.stickersList.map((sticker, index) => <img src={sticker.stickerSrc} alt={sticker.stickerName} key={`${col.colName} ${sticker.stickerName} ${index}`} />)}
+                {col.stickersList.map((sticker, index) =>
+                    <a href={getStickerMarketLink(sticker.stickerName)} key={`Link ${col.colName} ${sticker.stickerName} ${index}`}>
+                        <img src={sticker.stickerSrc} alt={sticker.stickerName} key={`Image ${col.colName} ${sticker.stickerName} ${index}`} />
+                    </a>
+                )}
             </Stack>
         </Stack>
     );
